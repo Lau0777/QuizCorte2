b3aac3e7727e89d3f417ba4d20eedadaf4e43c52
Laura <lauyessi0708@gmail.com>
2024-10-28 09:38:44 -0500

    first commit

--- src/main/java/com/example/demo/Repository/RMascotas.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.Model.Mascotas;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RMascotas extends JpaRepository<Mascotas, Integer> {
-}
